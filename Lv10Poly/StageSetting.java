5795c8ea31119f627e341ebf74d5c3dee069d719
Glosvogory <Glosvogory@kildultoy>
2021-10-27 17:40:01 +0900

    분석

---
+++ Lv10Poly/StageSetting.java
@@ -0,0 +1,15 @@
+public class StageSetting extends Stage{
+
+	@Override
+	public boolean update() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Lv10Poly/StageTitle.java
@@ -0,0 +1,18 @@
+public class StageTitle extends Stage{
+
+	@Override
+	public boolean update() {
+		System.out.println("==== TEXT RPG ====");
+		System.out.println("[시작] 을 입력하세요");
+		String start = GameManager.scan.next();
+		if(start.equals("시작"));
+		GameManager.nextStage = "LOBBY";
+		return false;
+	}
+
+	@Override
+	public void init() {
+		
+	}
+
+}
