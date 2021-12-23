e0f1f8a04114d2e1f95dd9598f5ff271fc0cd5af
Administrator <Administrator@msdn-group1>
2021-12-23 17:14:23 +0900

    로그인 회원가입 DB연동완료

--- webEx_Join/src/main/webapp/database/testPing.sql
+++ webEx_Join/src/main/webapp/database/testPing.sql
@@ -1,2 +1,28 @@
+
+create schema loginServer;
+
+use loginServer;
+
+create table users(
+	
+	code integer primary key auto_increment,
+	id varchar(30) not null,
+	pw varchar(30) not null,
+	
+	regDate datetime not null default current_timestamp
+
+
+);
+
+
 select* from users;
 
+use loginServer;
+insert users(id, pw) values('test', '1234');
+insert users(id, pw) values('zxcv','1234');
+
+
+drop table users;
+
+
+delete from users where code = 2;
